b13c11d78340bdeab6bff9e23ea71ad3ba4a519e
Kevin Harrison <kharrison7@elon.edu>
2017-09-17 12:54:19 -0400

    This adds a rotating react icon.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../styles/App.css';
 
 export default class NavBar extends Component {
   render() {
     return (
       <div className="header">
+        <img src={logo} className="App-logo" alt="logo" />
       Play What? - Update Count: {this.props.count}
       </div>
     );
