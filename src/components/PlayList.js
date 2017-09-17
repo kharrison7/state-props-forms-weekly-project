2d5abfa5d3a9c45ed2cf1fe7fe58a9302952e84e
Kevin Harrison <kharrison7@elon.edu>
2017-09-17 16:51:31 -0400

    This gets data from clicking the update button and sends a music snippet to the audio player.

--- src/components/PlayList.js
+++ src/components/PlayList.js
@@ -20,6 +20,7 @@ export class PlayList extends Component {
          return results.json();
        })
          .then(data => {
+           console.log(data);
            this.setState({song: data});
            console.log("state", this.state.songs);
          })
@@ -34,7 +35,65 @@ export class PlayList extends Component {
    })
    .then(data => {
      this.setState({songs: data});
-     console.log(this.state.songs);
+    //  console.log(this.state.songs);
+     let songItem = this.state.songs.map(song => {
+        // console.log(song.songTitle);
+        let songName = song.songTitle;
+        let space_Count = songName.split(" ").length-1;
+        //This removes all spaces and replaces them with '+'
+        let refined_songName = [];
+        if (space_Count > 0){
+          for(let i=0; i < space_Count; i++){
+             if( i === 0 ){
+               refined_songName[i] = songName.replace(" ", "+");
+             }
+             else{
+               refined_songName[i] = refined_songName[i-1].replace(" ", "+");
+             }
+          }
+        }
+        else{
+        space_Count = 1;
+        refined_songName[0] = songName;
+        }
+        let x = 'https://itunes.apple.com/search?term=';
+        let z = "&entity=song";
+        let y = x + refined_songName[space_Count - 1] + z;
+        // console.log(refined_songName);
+        console.log(y);
+        // This fetches the information using the url obtained above and returns that data to the browser.
+        fetch(y)
+          .then(
+            function(response) {
+              if (response.status !== 200) {
+                console.log('Looks like there was a problem. Status Code: ' +
+                  response.status);
+                return;
+              }
+              else{
+                console.log('fetch successfully.');
+              }
+              // This puts the various data on the browser page.
+              response.json().then(function(data) {
+                // This makes an array to fill with music.
+                let aud = [];
+                // This goes through the results and finds the top result.
+                    let result = data.results[0];
+                    aud[0] = new Audio(result.previewUrl);
+                // This sends the first song returned before any song is clicked.
+                let play_Song = document.getElementById('music_Here');
+                play_Song.src=aud[0].src;
+                play_Song.load();
+           })
+           .catch(function(err) {
+            console.log("Fetch Error: ", err);
+           });
+          });
+
+     });
+
+
+
    })
   }
 
