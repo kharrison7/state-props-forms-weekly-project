3010f63ad8057fbd150e71c86ee7644570b77b61
Kevin Harrison <kharrison7@elon.edu>
2017-09-18 13:19:13 -0400

    This makes some improvements to the Go Down Current Song button functionality.

--- src/components/PlayListItem.js
+++ src/components/PlayListItem.js
@@ -6,50 +6,54 @@ export default class PlayListItem extends Component {
   constructor(props) {
       super(props);
       this.state = {
-        songCount: 0,
+        songCount: 1,
         songListed: 0
       };
 
   this.handleSongSubmit = this.handleSongSubmit.bind(this);
 }
 
-handleSongSubmit(event){
-  console.log("Event: "+event.currentTarget.id);
-
-    this.setState({songListed: event.currentTarget.id});
-  if (this.state.songCount < 9) {
+handleSongSubmit(event) {
+  console.log("Event: " + event.currentTarget.id);
+this.setState({songListed: event.currentTarget.id});
+let audioList = this.props.audioList;
+if (event.currentTarget.id === 'countDown') {
+  if (this.state.songCount < 11) {
     this.setState({
       songCount: this.state.songCount + 1
     });
   } else {
-    this.setState({songCount: 0});
+    this.setState({songCount: 1});
   }
-  let audioList = this.props.audioList;
-  if(audioList[this.state.songCount] !== undefined){
+  if (audioList[this.state.songCount-1] !== undefined) {
     let play_Song = document.getElementById('music_Here');
-    play_Song.src=audioList[this.state.songCount].src;
+    play_Song.src = audioList[this.state.songCount-1].src;
     play_Song.load();
-  } else {
-    this.setState({songCount: this.state.songCount + 1});
   }
-
-if(event.currentTarget.id !== 'countDown'){
-console.log("x");
-this.setState({songCount: event.currentTarget.id});
-if(audioList[event.currentTarget.id-1] !== undefined){
-  let play_Song = document.getElementById('music_Here');
-  play_Song.src=audioList[event.currentTarget.id-1].src;
-  play_Song.load();
 }
-
+// if (audioList[this.state.songCount] !== undefined) {
+//   let play_Song = document.getElementById('music_Here');
+//   play_Song.src = audioList[this.state.songCount].src;
+//   play_Song.load();
+// } else {
+//   this.setState({
+//     songCount: this.state.songCount + 1
+//   });
+// }
+if (event.currentTarget.id !== 'countDown') {
+  console.log("x");
+  this.setState({songCount: event.currentTarget.id});
+  if (audioList[event.currentTarget.id - 1] !== undefined) {
+    let play_Song = document.getElementById('music_Here');
+    play_Song.src = audioList[event.currentTarget.id - 1].src;
+    play_Song.load();
+  }
 }
-
-
-
-
-
-  console.log(this.state.songListed);
-  console.log(this.state.songCount);
+if (this.state.songCount > 10) {
+  this.setState({songCount: 1});
+}
+console.log(this.state.songListed);
+console.log(this.state.songCount);
 }
 
     render() {
@@ -65,7 +69,6 @@ if(audioList[event.currentTarget.id-1] !== undefined){
                 <h6 className="card-subtitle mb-2">Title: {song.songTitle}</h6>
                 <h6 className="card-subtitle mb-2">Song Notes: {song.songNotes}</h6>
                 <h6 className="card-subtitle mb-2">Song Number: {countUp}</h6>
-
               </div>
             </div>
           </div>
@@ -74,7 +77,7 @@ if(audioList[event.currentTarget.id-1] !== undefined){
         return (
           <div className="containerItem">
             <form className="button">
-              <button onClick={this.handleSongSubmit}  id="countDown" type="button" className="btn btn-success">Go Down/Current Song: {this.state.songCount}</button>
+              <button onClick={this.handleSongSubmit}  id="countDown" type="button" className="btn btn-success">Go Down/Current Song: {this.state.songCount-1}</button>
             </form>
             {songItem}
           </div>
